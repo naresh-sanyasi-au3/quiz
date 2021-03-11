6054eb2bf0bb195bb1b7fe45925da330ffea6a8c
naresh-sanyasi-au3 <naresspp@gmail.com>
2021-03-11 19:50:35 +0530

    changd

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('build'));
-    const path = require('path');
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-};
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static('build'));
+//     const path = require('path');
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     });
+// };
+
+app.use(express.static('build'));
+const path = require('path');
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log("server is running");
 });
